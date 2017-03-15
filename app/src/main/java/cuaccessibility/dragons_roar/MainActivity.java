cf5fb548d7084b82d215ca0efe4efb28cc1eb89d
e-lloyd <evan.a.lloyd@colorado.edu>
2017-03-15 17:11:24 -0600

    Sort of blanking here - made a blank activity and set permissions we need.

--- app/src/main/java/cuaccessibility/dragons_roar/MainActivity.java
+++ app/src/main/java/cuaccessibility/dragons_roar/MainActivity.java
@@ -2,6 +2,10 @@ package cuaccessibility.dragons_roar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //
 }
