4a067686d4606f57ba1a92cee84e64748f7ccd78
Matt Bing <Matthew.h.bing@gmail.com>
2017-04-26 11:41:16 -0600

    Text input working fully

--- app/src/main/java/cuaccessibility/dragons_roar/voiceButton.java
+++ app/src/main/java/cuaccessibility/dragons_roar/voiceButton.java
@@ -3,6 +3,7 @@ package cuaccessibility.dragons_roar;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.support.annotation.NonNull;
@@ -10,26 +11,42 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
+import ai.api.AIServiceException;
+import ai.api.RequestExtras;
 import ai.api.android.AIConfiguration;
+import ai.api.android.AIDataService;
+import ai.api.android.AIService;
 import ai.api.android.GsonFactory;
+import ai.api.model.AIContext;
 import ai.api.model.AIError;
+import ai.api.model.AIEvent;
+import ai.api.model.AIRequest;
 import ai.api.model.AIResponse;
 import ai.api.model.Metadata;
 import ai.api.model.Result;
 import ai.api.model.Status;
 import ai.api.ui.AIButton;
+
+
 
 import static java.lang.Integer.valueOf;
 /*
@@ -48,7 +65,21 @@ public class voiceButton extends AppCompatActivity implements AIButton.AIButtonL
     public static final String TAG = voiceButton.class.getName();
 
     private AIButton aiButton;
+
+    private AIService service;
+
+    private TextView queryEditText;
+
+
+    private CheckBox eventCheckBox;
+    private Spinner eventSpinner;
+    private AIDataService aiDataService;
+    private EditText contextEditText;
+
+
     private TextView resultTextView;
+    String resultsLog;
+
     private static final int REQUEST_AUDIO_PERMISSIONS_ID = 33;
     private Gson gson = GsonFactory.getGson();
 
@@ -65,7 +96,14 @@ public class voiceButton extends AppCompatActivity implements AIButton.AIButtonL
         checkAudioRecordPermission();
 
         resultTextView = (TextView) findViewById(R.id.resultTextView);
+        resultTextView.setSingleLine(false);
+        resultsLog = "";
+
+        queryEditText = (EditText) findViewById(R.id.textQuery);
+
         aiButton = (AIButton) findViewById(R.id.micButton);
+
+        service = aiButton.getAIService();
 
         //Added by Matt: This resultHandler is what will be used to load the character.
         //After we implement character loading/saving, it should take a CharacterSheet as an arg.
@@ -77,9 +115,74 @@ public class voiceButton extends AppCompatActivity implements AIButton.AIButtonL
 
         aiButton.initialize(config);
         aiButton.setResultsListener(this);
-
-
+        aiDataService = new AIDataService(this, config);
     }
+
+
+    private void sendRequest() {
+
+        final String queryString = String.valueOf(queryEditText.getText());
+        //final String eventString = eventSpinner.isEnabled() ? String.valueOf(String.valueOf(eventSpinner.getSelectedItem())) : null;
+        //final String contextString = String.valueOf(contextEditText.getText());
+
+        //if (TextUtils.isEmpty(queryString) && TextUtils.isEmpty(eventString)) {
+        //onError(new AIError(getString(R.string.non_empty_query)));
+        //return;
+        //}
+
+        final AsyncTask<String, Void, AIResponse> task = new AsyncTask<String, Void, AIResponse>() {
+
+            private AIError aiError;
+
+            @Override
+            protected AIResponse doInBackground(final String... params) {
+                final AIRequest request = new AIRequest();
+                String query = params[0];
+                //String event = params[1];
+
+                if (!TextUtils.isEmpty(query))
+                    request.setQuery(query);
+                //if (!TextUtils.isEmpty(event))
+                //request.setEvent(new AIEvent(event));
+                //final String contextString = params[2];
+                //RequestExtras requestExtras = null;
+                //if (!TextUtils.isEmpty(contextString)) {
+                //final List<AIContext> contexts = Collections.singletonList(new AIContext(contextString));
+                //requestExtras = new RequestExtras(contexts, null);
+                //}
+
+                try {
+                    return aiDataService.request(request);
+                } catch (final AIServiceException e) {
+                    aiError = new AIError(e);
+                    return null;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(final AIResponse response) {
+                if (response != null) {
+                    onResult(response);
+                } else {
+                    onError(aiError);
+                }
+            }
+        };
+
+        task.execute(queryString);
+    }
+
+
+    public void buttonSend(View view){
+        sendRequest();
+    }
+
+    public void buttonClear(View view){
+        resultTextView.setText("");
+    }
+
+
+
     protected void checkAudioRecordPermission() {
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.RECORD_AUDIO)
@@ -142,47 +245,58 @@ public class voiceButton extends AppCompatActivity implements AIButton.AIButtonL
     }
 
 
+
+
     @Override
     public void onResult(final AIResponse response) {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                Log.d(TAG, "onResult");
+                //Log.d(TAG, "onResult");
 
-                resultTextView.setText(gson.toJson(response));
+                //resultTextView.setText(gson.toJson(response));
 
-                Log.i(TAG, "Received success response");
+                //Log.i(TAG, "Received success response");
 
                 // this is example how to get different parts of result object
                 final Status status = response.getStatus();
-                Log.i(TAG, "Status code: " + status.getCode());
-                Log.i(TAG, "Status type: " + status.getErrorType());
+                //Log.i(TAG, "Status code: " + status.getCode());
+                //Log.i(TAG, "Status type: " + status.getErrorType());
 
                 final Result result = response.getResult();
-                Log.i(TAG, "Resolved query: " + result.getResolvedQuery());
+                //Log.i(TAG, "Resolved query: " + result.getResolvedQuery());
 
-                Log.i(TAG, "Action: " + result.getAction());
-                final String speech = result.getFulfillment().getSpeech();
-                Log.i(TAG, "Speech: " + speech);
+                //Log.i(TAG, "Action: " + result.getAction());
+                //final String speech = result.getFulfillment().getSpeech();
+                //Log.i(TAG, "Speech: " + speech);
                 //TTS.speak(speech);
 
                 final HashMap<String, JsonElement> params = result.getParameters();
+                final Metadata metadata = result.getMetadata();
+
+                /*
                 if (params != null && !params.isEmpty()) {
                     Log.i(TAG, "Parameters: ");
                     for (final Map.Entry<String, JsonElement> entry : params.entrySet()) {
                         Log.i(TAG, String.format("%s: %s", entry.getKey(), entry.getValue().toString()));
                     }
-                }
-                final Metadata metadata = result.getMetadata();
+                }*/
+
+
 
                 String fullResponseForUser = thisQuery.getResponse(params, metadata);
-                Snackbar mySnackbar = Snackbar.make(findViewById(R.id.CoordLayout), fullResponseForUser, Snackbar.LENGTH_INDEFINITE);
-                mySnackbar.show();
+                resultsLog = fullResponseForUser + " \n" + resultsLog;
+                resultTextView.setText(resultsLog);
+                //Snackbar mySnackbar = Snackbar.make(findViewById(R.id.CoordLayout), fullResponseForUser, Snackbar.LENGTH_INDEFINITE);
+                //mySnackbar.show();
 
             }
 
         });
     }
+
+
+
 
     @Override
     public void onError(final AIError error) {
